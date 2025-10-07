5c4d6688a1b77c8afe9addb791b4b0fa028049ef
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-07 23:29:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7988359152:AAFTDV1NFLuO_tUldwbTP_H2ZtIfp_SDFqo")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 API_ID = int(os.getenv("API_ID", "29308061"))
 API_HASH = os.getenv("API_HASH", "462de3dfc98fd938ef9c6ee31a72d099")
 
 WORDS_PATH = os.getenv("WORDS_PATH", "words.txt")
 DB_PATH = os.getenv("DB_PATH", "sessions.db")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "8315954262"))
+OWNER_ID = int(os.getenv("OWNER_ID", "8157752411"))
 SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/TNCmeetup")
 SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/TechNodeCoders")
 START_IMAGE = os.getenv("START_IMAGE", "assets/start_banner.jpg")
